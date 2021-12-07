27d00cc982d1cec67894f7d46557003e27cb9672
edwin2207 <edwined18@gmail.com>
2021-12-07 13:00:44 +0530

    Password Functionality

--- src/org/test/Login.java
+++ src/org/test/Login.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class Login {
+public static void password() {
+	System.out.println("Password Functionality");
+
+}
+public static void main(String[] args) {
+    password();
+}
 
 }
